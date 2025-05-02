59ff2768c1f5010d3ba23617a564e9711f32742e
Hakan Balas <10181303+hakanbalas@users.noreply.github.com>
2025-05-02 22:15:00 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'LupusOS Documentation'
+copyright = '2025, LupusOS Team'
+author = 'LupusOS Team'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
 
